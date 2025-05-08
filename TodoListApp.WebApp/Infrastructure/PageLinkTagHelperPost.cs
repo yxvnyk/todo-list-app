950833e04ec43592977de4172af0436f64b66eb8
yxvnyk <gobbleddygook92@gmail.com>
2025-05-08 19:37:54 +0300

    ui: improve styes
    Improve visualisation of all site
    
    -change layouts
    -sidebar
    -navbar
    -creta/edit/deatais pages

--- TodoListApp.WebApp/Infrastructure/PageLinkTagHelperPost.cs
+++ TodoListApp.WebApp/Infrastructure/PageLinkTagHelperPost.cs
@@ -1,6 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using TodoListApp.WebApi.Models.DTO.PagingDTO;
@@ -10,11 +8,8 @@ namespace TodoListApp.WebApp.Infrastructure
     [HtmlTargetElement("div", Attributes = "page-model-post")]
     public class PageLinkTagHelperPost : TagHelper
     {
-        private readonly IUrlHelperFactory urlHelperFactory;
-
-        public PageLinkTagHelperPost(IUrlHelperFactory helperFactory)
+        public PageLinkTagHelperPost()
         {
-            this.urlHelperFactory = helperFactory;
         }
 
         [ViewContext]
@@ -23,8 +18,6 @@ namespace TodoListApp.WebApp.Infrastructure
 
         [HtmlAttributeName("page-model-post")]
         public TaskPaging? PageModelPost { get; set; }
-
-        public TaskPaging? PageFilter { get; set; }
 
         public string PageAction { get; set; } = "GetByFilter";
 
@@ -38,32 +31,32 @@ namespace TodoListApp.WebApp.Infrastructure
 
         public string PageClassSelected { get; set; } = "btn-primary";
 
-        public string? PageRoute { get; set; }
-
         [HtmlAttributeName(DictionaryAttributePrefix = "page-post-")]
         public Dictionary<string, object> PagePostValues { get; } = new Dictionary<string, object>();
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             ArgumentNullException.ThrowIfNull(output);
+
             if (this.ViewContext != null && this.PageModelPost != null)
             {
-                IUrlHelper urlHelper = this.urlHelperFactory.GetUrlHelper(this.ViewContext);
                 TagBuilder result = new TagBuilder("div");
+
                 for (int i = 1; i <= this.PageModelPost.TotalCount; i++)
                 {
                     TagBuilder form = new TagBuilder("form");
                     form.Attributes["method"] = "post";
-                    form.Attributes["action"] = form.Attributes["action"] = "/" + this.PageController + "/" + this.PageAction;
-
+                    form.Attributes["action"] = "/" + this.PageController + "/" + this.PageAction;
                     form.AddCssClass("d-inline");
 
+                    // Add hidden input for the page number
                     TagBuilder pageNumber = new TagBuilder("input");
                     pageNumber.Attributes["type"] = "hidden";
                     pageNumber.Attributes["name"] = "PageNumber";
                     pageNumber.Attributes["value"] = i.ToString();
                     _ = form.InnerHtml.AppendHtml(pageNumber);
 
+                    // Add any additional post values
                     foreach (var pair in this.PagePostValues)
                     {
                         TagBuilder input = new TagBuilder("input");
@@ -73,20 +66,21 @@ namespace TodoListApp.WebApp.Infrastructure
                         _ = form.InnerHtml.AppendHtml(input);
                     }
 
+                    // Create the button that looks like a link
                     TagBuilder button = new TagBuilder("button");
                     button.Attributes["type"] = "submit";
                     _ = button.InnerHtml.Append(i.ToString());
-                    form.InnerHtml.AppendHtml(button);
 
-                    //this.PageUrlValues[key: "page"] = i;
-
-                    _ = result.InnerHtml.AppendHtml(form);
                     if (this.PageClassesEnabled)
                     {
-                        form.AddCssClass(this.PageClass);
-                        form.AddCssClass(i == this.PageModelPost.CurrentPage
+                        button.AddCssClass(this.PageClass);
+                        button.AddCssClass(i == this.PageModelPost.CurrentPage
                             ? this.PageClassSelected : this.PageClassNormal);
                     }
+
+                    _ = form.InnerHtml.AppendHtml(button);
+
+                    _ = result.InnerHtml.AppendHtml(form);
                 }
 
                 _ = output.Content.AppendHtml(result.InnerHtml);
